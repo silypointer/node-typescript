275c8b7b480cc90e629a04a6f18665ad7bf72bd0
Rohan <rohanbpatil77@gmail.com>
2021-07-28 01:19:17 +0530

    TypeScripting

--- app.ts
+++ app.ts
@@ -1,5 +1,10 @@
-import express from "express";
+import express from 'express';
+import bodyParser from 'body-parser';
+import todosRoutes from './routes/todos';
 
-const app = express()
+const app = express();
 
-app.listen(2020)
+app.use(bodyParser.json());
+app.use(todosRoutes);
+
+app.listen(2020);
